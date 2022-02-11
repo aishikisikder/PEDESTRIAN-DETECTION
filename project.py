45dc00f23e27faee78f507ba6a052dd86a585753
aishikisikder <91593104+aishikisikder@users.noreply.github.com>
2022-02-11 17:24:44 +0530

    Update project.py

--- project.py
+++ project.py
@@ -10,21 +10,15 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 image = cv2.imread("C:\\Users\\User\\Downloads\\ped3.jpg")
 
 # Resizing the Image
-image = imutils.resize(image,
-					width=min(400, image.shape[1]))
+image = imutils.resize(image,width=min(400, image.shape[1]))
 
 # Detecting all the regions in the
 # Image that has a pedestrians inside it
-(regions, _) = hog.detectMultiScale(image,
-									winStride=(4, 4),
-									padding=(4, 4),
-									scale=1.05)
+(regions, _) = hog.detectMultiScale(image,winStride=(4, 4),padding=(4, 4),scale=1.05)
 
 # Drawing the regions in the Image
 for (x, y, w, h) in regions:
-	cv2.rectangle(image, (x, y),
-				(x + w, y + h),
-				(0, 0, 255), 2)
+	cv2.rectangle(image, (x, y),(x + w, y + h),(0, 0, 255), 2)
 
 # Showing the output Image
 cv2.imshow("Image", image)
